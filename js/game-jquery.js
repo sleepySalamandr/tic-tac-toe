53e42d9170306a8881367deb555849a981e437cf
Brittany Crocker <brittany.crocker@outlook.com>
2020-10-01 14:00:24 +1000

    Cleaned up code by adding to objects

--- js/game-jquery.js
+++ js/game-jquery.js
@@ -3,72 +3,64 @@ $(document).ready(function(){
 
 const gameFunction = function(e) {
 
-  // Naughts and crosses img
+// Naughts and crosses img
   let cross = $('<img>').attr("src", "css/img/x-64.png");
-  let naught = $('<img>').attr("src", "css/img/naught.png")
+  let naught = $('<img>').attr("src", "css/img/naught.png");
 
   clickCounter ++
 
 // Class of square clicked converted from string to className
-    let clickedLocationString = $(event.target).attr('class')
-    let clickedLocationClassName = "." + clickedLocationString.split(" ").join(".")
+    let clickedLocationString = $(event.target).attr('class');
+    let clickedLocationClassName = "." + clickedLocationString.split(" ").join(".");
 
 
-// Display naught or cross
+// Display naught or cross / pushes the number (id) of the box to the array / pushes the clikecd location to an array containing all clicked locations
     if (clickCounter % 2 === 0) {
 
-      // pushes the number of the box to the array
-      playerChoices.playerTwo.push(parseInt($(event.target).attr("id")))
-
-      // Display naught or cross
-      clickedLocations.push(clickedLocationClassName)
-      $(clickedLocationClassName).append(naught).html(naught)
-
+      // playerChoices.playerTwo.push(parseInt($(event.target).attr("id")));
+      playerTwo.choices.push(parseInt($(event.target).attr("id")));
+      $(clickedLocationClassName).append(naught).html(naught);
+      clickedLocations.push(clickedLocationClassName);
 
     } else {
-      playerChoices.playerOne.push(parseInt($(event.target).attr("id")))
-      clickedLocations.push(clickedLocationClassName)
-      $(clickedLocationClassName).append(cross).html(cross)
-    }
 
-// Play again - resets the board
+      // playerChoices.playerOne.push(parseInt($(event.target).attr("id")));
+      playerOne.choices.push(parseInt($(event.target).attr("id")));
+      clickedLocations.push(clickedLocationClassName);
+      $(clickedLocationClassName).append(cross).html(cross);
+
+    };
+
+// Play again button - resets the board
     const playAgain = function (e) {
-      playerChoices.playerOne = []
-      playerChoices.playerTwo = []
-      clickCounter = 0
 
-      $(clickedLocations).each(function(index){
-        $(naught).detach()
-        $(cross).detach()
-        $(".winnerAlert").text(``)
-        $(".board-container").removeClass("harder")
-        $(".board-container").removeClass("disable-click");
-        $(".board-container").removeClass("draw")
+// Resets arrays
+      playerOne.choices = [];
+      playerTwo.choices = [];
+      // playerChoices.playerOne = [];
+      // playerChoices.playerTwo = [];
+      clickCounter = 0;
 
+// Resets HTML
+      $(".winnerAlert").text(``)
+      $(".board-container").removeClass("harder")
+      $(".board-container").removeClass("disable-click");
 
+      $(clickedLocations).each( function(index) {
+        $(naught).detach();
+        $(cross).detach();
       });
-    }
+
+  };
     $('.reset').on("click", playAgain)
 
+
 findWinner()
-console.log(clickedLocationClassName)
   }
   $(".board-container").on("click", gameFunction)
 
 
   // finds if wins
-
-
-
-const harder = function(e) {
-  $(".board-container").addClass("harder")
-  console.log("clicked")
-
-}
-
-
-$("#harder").on("click", harder)
-
 
 
 });
--- js/game-js.js
+++ js/game-js.js
@@ -13,10 +13,10 @@ const winning = {
 }
 
 // Records the id of the clicked location - the id is a number.
-const playerChoices = {
-  playerOne: [],
-  playerTwo: [],
-}
+// const playerChoices = {
+//   playerOne: [],
+//   playerTwo: [],
+// }
 
 const clickedLocations = []
 
@@ -26,30 +26,58 @@ const wins = {
   playerTwo: 0
 }
 
+playerOne = {
+  choices: [],
+  winnerAlert: `Player 1 Wins!`,
+};
+
+playerTwo = {
+  choices: [],
+  winnerAlert: `Player 2 Wins!`,
+};
+
 // number of clicks
 let clickCounter = 0;
 
-const findWinner = function() {  for ( let keys in winning) {
+const findWinner = function() {
+  for ( let keys in winning) {
 
   if  (clickCounter >= 9) {
   $(".winnerAlert").html(`It's a draw!`)
 
   }
 
-if (winning[keys].every(numberOfSquare => playerChoices.playerOne.includes(numberOfSquare))) {
+if (winning[keys].every(numberOfSquare =>
+  playerOne.choices.includes(numberOfSquare))) {
+// if (winning[keys].every(numberOfSquare => playerChoices.playerOne.includes(numberOfSquare))) {
     wins.playerOne = wins.playerOne + 1
-    $(".winnerAlert").text(`Player 1 Wins!`)
-    $(".player1-score").html(`Player 1: <br/> ${wins.playerOne}`)
+    // $(".winnerAlert").text(`Player 1 Wins!`)
+    $(".winnerAlert").text(`${playerOne.winnerAlert}`)
+    // $(".player1-score").html(`Player 1: <br/> ${wins.playerOne}`)
+      $(".player1-score").html(`Player 1: <br/> ${wins.playerOne}`)
     $('.board-container').addClass("disable-click")
     return;
 
-  } else if (winning[keys].every(numberOfSquare => playerChoices.playerTwo.includes(numberOfSquare))) {
+  } else if (winning[keys].every(numberOfSquare =>
+    playerTwo.choices.includes(numberOfSquare))) {
+  // } else if (winning[keys].every(numberOfSquare => playerChoices.playerTwo.includes(numberOfSquare))) {
     // window.alert('Player two wins')
     wins.playerTwo = wins.playerTwo + 1
-    $(".winnerAlert").text(`Player 2 Wins!`)
+    // $(".winnerAlert").text(`Player 2 Wins!`)
+    $(".winnerAlert").text(`${playerTwo.winnerAlert}`)
+    // $(".player2-score").html(`Player 2: <br/> ${wins.playerTwo}`)
     $(".player2-score").html(`Player 2: <br/> ${wins.playerTwo}`)
     $('.board-container').addClass("disable-click")
     return;
   }
 }
 }
+
+// Make harder button / makes board spin
+const harder = function(e) {
+
+  $(".board-container").addClass("harder")
+
+}
+
+$("#harder").on("click", harder)
